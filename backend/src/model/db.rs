33da68acfb82839a8193f54de4c7316d62afd395
Jeremy Chone <jeremy.chone@gmail.com>
2021-12-11 15:41:10 -0800

    021

--- backend/src/model/db.rs
+++ backend/src/model/db.rs
@@ -2,7 +2,16 @@ use sqlx::postgres::PgPoolOptions;
 use sqlx::{Pool, Postgres};
 use std::time::Duration;
 
+const PG_HOST: &str = "localhost";
+const PG_ROOT_DB: &str = "postgres";
+const PG_ROOT_USER: &str = "postgres";
+const PG_ROOT_PWD: &str = "postgres";
+
 pub type Db = Pool<Postgres>;
+
+pub async fn init_db() -> Result<Db, sqlx::Error> {
+	new_db_pool(PG_HOST, PG_ROOT_DB, PG_ROOT_USER, PG_ROOT_PWD, 1).await
+}
 
 async fn new_db_pool(host: &str, db: &str, user: &str, pwd: &str, max_con: u32) -> Result<Db, sqlx::Error> {
 	let con_string = format!("postgres://{}:{}@{}/{}", user, pwd, host, db);
