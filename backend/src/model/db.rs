d099788eb9e9f5b0633616a72f2a22387564e03d
Jeremy Chone <jeremy.chone@gmail.com>
2021-12-11 15:38:59 -0800

    020

--- backend/src/model/db.rs
+++ backend/src/model/db.rs
@@ -1,3 +1,14 @@
+use sqlx::postgres::PgPoolOptions;
 use sqlx::{Pool, Postgres};
+use std::time::Duration;
 
 pub type Db = Pool<Postgres>;
+
+async fn new_db_pool(host: &str, db: &str, user: &str, pwd: &str, max_con: u32) -> Result<Db, sqlx::Error> {
+	let con_string = format!("postgres://{}:{}@{}/{}", user, pwd, host, db);
+	PgPoolOptions::new()
+		.max_connections(max_con)
+		.connect_timeout(Duration::from_millis(500))
+		.connect(&con_string)
+		.await
+}
