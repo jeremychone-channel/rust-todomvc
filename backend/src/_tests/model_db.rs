1f03299d8dfd08b62e025c4717c880e8153cfb25
Jeremy Chone <jeremy.chone@gmail.com>
2021-12-12 10:47:25 -0800

    039

--- backend/src/_tests/model_db.rs
+++ backend/src/_tests/model_db.rs
@@ -2,7 +2,12 @@ use super::init_db;
 
 #[tokio::test]
 async fn model_db_init_db() -> Result<(), Box<dyn std::error::Error>> {
+	// ACTION
 	let db = init_db().await?;
+
+	// CHECK
+	let result = sqlx::query("SELECT * from todo").fetch_all(&db).await?;
+	assert_eq!(2, result.len(), "number of seed todos");
 
 	Ok(())
 }
