40d5d7eff6bf824f0192c80a019599bbbe211fa8
Jeremy Chone <jeremy.chone@gmail.com>
2021-12-11 17:48:45 -0800

    027

---
+++ backend/sql/01-create-schema.sql
@@ -0,0 +1,15 @@
+-- Todo status enum
+CREATE TYPE todo_status_enum AS ENUM (
+  'open',
+  'close'
+);
+
+-- Todo
+CREATE TABLE todo (
+  id bigserial,
+  cid bigint NOT NULL, -- creator user id
+	ctime timestamp with time zone DEFAULT now(),
+  title text NOT NULL,
+  status todo_status_enum NOT NULL DEFAULT 'open'
+);
+ALTER SEQUENCE todo_id_seq RESTART WITH 1000;
