8f8fb6d1e86bdb9eec4163fb84b18128f4f3636b
Jeremy Chone <jeremy.chone@gmail.com>
2021-12-12 10:11:24 -0800

    031

--- backend/src/model/db.rs
+++ backend/src/model/db.rs
@@ -13,6 +13,10 @@ pub async fn init_db() -> Result<Db, sqlx::Error> {
 	new_db_pool(PG_HOST, PG_ROOT_DB, PG_ROOT_USER, PG_ROOT_PWD, 1).await
 }
 
+async fn pexec(db: &Db, file: &str) -> Result<(), sqlx::Error> {
+	Ok(())
+}
+
 async fn new_db_pool(host: &str, db: &str, user: &str, pwd: &str, max_con: u32) -> Result<Db, sqlx::Error> {
 	let con_string = format!("postgres://{}:{}@{}/{}", user, pwd, host, db);
 	PgPoolOptions::new()
