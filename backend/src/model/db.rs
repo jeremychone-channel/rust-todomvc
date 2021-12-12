18eefebc355807b89ce671b8698c125bb08f78d0
Jeremy Chone <jeremy.chone@gmail.com>
2021-12-12 10:44:11 -0800

    038

--- backend/src/model/db.rs
+++ backend/src/model/db.rs
@@ -1,6 +1,7 @@
 use sqlx::postgres::PgPoolOptions;
 use sqlx::{Pool, Postgres};
 use std::fs;
+use std::path::PathBuf;
 use std::time::Duration;
 
 const PG_HOST: &str = "localhost";
@@ -23,6 +24,23 @@ pub async fn init_db() -> Result<Db, sqlx::Error> {
 	{
 		let root_db = new_db_pool(PG_HOST, PG_ROOT_DB, PG_ROOT_USER, PG_ROOT_PWD, 1).await?;
 		pexec(&root_db, SQL_RECREATE).await?;
+	}
+
+	// -- Run the app sql files
+	let app_db = new_db_pool(PG_HOST, PG_APP_DB, PG_APP_USER, PG_APP_PWD, 1).await?;
+	let mut paths: Vec<PathBuf> = fs::read_dir(SQL_DIR)?
+		.into_iter()
+		.filter_map(|e| e.ok().map(|e| e.path()))
+		.collect();
+	paths.sort();
+	// execute each file
+	for path in paths {
+		if let Some(path) = path.to_str() {
+			// only .sql and not the recreate
+			if path.ends_with(".sql") && path != SQL_RECREATE {
+				pexec(&app_db, &path).await?;
+			}
+		}
 	}
 
 	// returning the app db
