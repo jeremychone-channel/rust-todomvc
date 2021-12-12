93422f84bd95fae225a4ac2d331069d0807586d9
Jeremy Chone <jeremy.chone@gmail.com>
2021-12-12 11:31:00 -0800

    050

--- backend/src/model/mod.rs
+++ backend/src/model/mod.rs
@@ -1,2 +1,13 @@
+use thiserror::Error as ThisError;
+
 mod db;
 mod todo;
+
+#[derive(ThisError, Debug)]
+pub enum Error {
+	#[error(transparent)]
+	SqlxError(#[from] sqlx::Error),
+
+	#[error(transparent)]
+	IOError(#[from] std::io::Error),
+}
