be04abac332c10c7a51241c6e42d4bcc527ea3e6
Jeremy Chone <jeremy.chone@gmail.com>
2021-12-12 11:05:47 -0800

    045

--- backend/src/model/todo.rs
+++ backend/src/model/todo.rs
@@ -1,3 +1,5 @@
+use super::db::Db;
+
 // region:    Todo Types
 #[derive(sqlx::FromRow, Debug, Clone)]
 pub struct Todo {
@@ -9,4 +11,17 @@ pub struct Todo {
 
 // region:    TodoMac
 pub struct TodoMac;
+
+impl TodoMac {
+	pub async fn list(db: &Db) -> Result<Vec<Todo>, sqlx::Error> {
+		let sql = "SELECT id, cid, title FROM todo ORDER BY id DESC";
+
+		// build the sqlx-query
+		let query = sqlx::query_as::<_, Todo>(&sql);
+		// execute the query
+		let todos = query.fetch_all(db).await?;
+
+		Ok(todos)
+	}
+}
 // endregion: TodoMac
