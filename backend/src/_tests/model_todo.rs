7f7c8cb6984338505fab61034e4c438086ce3b25
Jeremy Chone <jeremy.chone@gmail.com>
2021-12-12 19:30:26 -0800

    079

--- backend/src/_tests/model_todo.rs
+++ backend/src/_tests/model_todo.rs
@@ -3,7 +3,7 @@ use crate::model::db::init_db;
 use crate::model::todo::{TodoPatch, TodoStatus};
 use crate::security::utx_from_token;
 
-use super::TodoMac;
+use super::{Todo, TodoMac};
 
 #[tokio::test]
 async fn model_todo_create() -> Result<(), Box<dyn std::error::Error>> {
@@ -111,6 +111,26 @@ async fn model_todo_list() -> Result<(), Box<dyn std::error::Error>> {
 	assert_eq!(100, todos[1].id);
 	assert_eq!(123, todos[1].cid);
 	assert_eq!("todo 100", todos[1].title);
+
+	Ok(())
+}
+
+#[tokio::test]
+async fn model_todo_delete_simple() -> Result<(), Box<dyn std::error::Error>> {
+	// -- FIXTURE
+	let db = init_db().await?;
+	let utx = utx_from_token("123").await?;
+
+	// -- ACTION
+	let todo = TodoMac::delete(&db, &utx, 100).await?;
+
+	// -- CHECK - deleted item
+	assert_eq!(100, todo.id);
+	assert_eq!("todo 100", todo.title);
+
+	// -- CHECK - list
+	let todos: Vec<Todo> = sqlb::select().table("todo").fetch_all(&db).await?;
+	assert_eq!(1, todos.len());
 
 	Ok(())
 }
