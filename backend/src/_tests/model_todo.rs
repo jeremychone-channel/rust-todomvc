113200d67c4b72e3e0737a68611086ad32d4688b
Jeremy Chone <jeremy.chone@gmail.com>
2021-12-12 16:34:13 -0800

    057

--- backend/src/_tests/model_todo.rs
+++ backend/src/_tests/model_todo.rs
@@ -1,6 +1,26 @@
 use crate::model::db::init_db;
+use crate::model::todo::TodoPatch;
 
 use super::TodoMac;
+
+#[tokio::test]
+async fn model_todo_create() -> Result<(), Box<dyn std::error::Error>> {
+	// -- FIXTURE
+	let db = init_db().await?;
+	let data_fx = TodoPatch {
+		title: Some("test - model_todo_create 1".to_string()),
+		..Default::default()
+	};
+
+	// -- ACTION
+	let todo_created = TodoMac::create(&db, data_fx.clone()).await?;
+
+	// -- CHECK
+	assert!(todo_created.id >= 1000, "Id should be >= 1000");
+	assert_eq!(data_fx.title.unwrap(), todo_created.title);
+
+	Ok(())
+}
 
 #[tokio::test]
 async fn model_todo_list() -> Result<(), Box<dyn std::error::Error>> {
