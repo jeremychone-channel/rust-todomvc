f2eefc11576510b38d38ad179db7533a0d413fce
Jeremy Chone <jeremy.chone@gmail.com>
2022-01-15 14:32:08 -0800

    106

--- backend/src/web/mod.rs
+++ backend/src/web/mod.rs
@@ -1,7 +1,15 @@
 use crate::model::Db;
+use std::path::Path;
 use std::sync::Arc;
 
 pub async fn start_web(web_folder: &str, web_port: u16, db: Arc<Db>) -> Result<(), Error> {
+	// validate web_folder
+	if !Path::new(web_folder).exists() {
+		return Err(Error::FailStartWebFolderNotFound(web_folder.to_string()));
+	}
+
+	// Static content
+
 	Ok(())
 }
 
