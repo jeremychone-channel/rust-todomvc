5771a855cc72230ba58725fcedd26951fa7e76a9
Jeremy Chone <jeremy.chone@gmail.com>
2022-01-15 15:32:19 -0800

    121

--- backend/src/web/todo.rs
+++ backend/src/web/todo.rs
@@ -2,7 +2,7 @@ use crate::model::Db;
 use std::sync::Arc;
 use warp::Filter;
 
-pub fn todo_rest_filters(
+pub fn todo_rest_filters<F>(
 	base_path: &'static str,
 	db: Arc<Db>,
 ) -> impl Filter<Extract = impl warp::Reply, Error = warp::Rejection> + Clone {
