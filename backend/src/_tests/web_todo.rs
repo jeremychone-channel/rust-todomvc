3802f78ba0907a57665eb9b2904193e199e25a04
Jeremy Chone <jeremy.chone@gmail.com>
2022-01-15 16:16:17 -0800

    130

--- backend/src/_tests/web_todo.rs
+++ backend/src/_tests/web_todo.rs
@@ -1,6 +1,25 @@
+use std::sync::Arc;
+
+use super::todo_rest_filters;
+use crate::model::init_db;
 use anyhow::Result;
 
 #[tokio::test]
 async fn web_todo_list() -> Result<()> {
+	// -- FIXTURE
+	let db = init_db().await?;
+	let db = Arc::new(db);
+	let todo_apis = todo_rest_filters("api", db.clone());
+
+	// -- ACTION
+	let resp = warp::test::request()
+		.method("GET")
+		.path("/api/todos")
+		.reply(&todo_apis)
+		.await;
+
+	// -- CHECK
+	assert_eq!(200, resp.status(), "http status");
+
 	Ok(())
 }
--- backend/src/web/todo.rs
+++ backend/src/web/todo.rs
@@ -6,7 +6,7 @@ use std::sync::Arc;
 use warp::reply::Json;
 use warp::{Filter, Rejection};
 
-pub fn todo_rest_filters<F>(
+pub fn todo_rest_filters(
 	base_path: &'static str,
 	db: Arc<Db>,
 ) -> impl Filter<Extract = impl warp::Reply, Error = warp::Rejection> + Clone {
