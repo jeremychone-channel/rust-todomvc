c32fa1bbcca99b741734c751a8c1c61448712796
Jeremy Chone <jeremy.chone@gmail.com>
2022-01-27 10:11:52 -0800

    139

--- backend/src/web/filter_auth.rs
+++ backend/src/web/filter_auth.rs
@@ -1,8 +1,25 @@
+use super::filter_utils::with_db;
 use crate::model::Db;
 use crate::security::{utx_from_token, UserCtx};
+use crate::web::Error;
 use std::sync::Arc;
 use warp::{Filter, Rejection};
 
-pub fn do_auth(_db: Arc<Db>) -> impl Filter<Extract = (UserCtx,), Error = Rejection> + Clone {
-	warp::any().and_then(|| async { Ok::<UserCtx, Rejection>(utx_from_token("123").await?) })
+const HEADER_XAUTH: &str = "X-Auth-Token";
+
+pub fn do_auth(db: Arc<Db>) -> impl Filter<Extract = (UserCtx,), Error = Rejection> + Clone {
+	// warp::any().and_then(|| async { Ok::<UserCtx, Rejection>(utx_from_token("123").await?) })
+
+	warp::any()
+		.and(with_db(db))
+		.and(warp::header::optional(HEADER_XAUTH))
+		.and_then(|db: Arc<Db>, xauth: Option<String>| async move {
+			match xauth {
+				Some(xauth) => {
+					let utx = utx_from_token(&xauth).await?;
+					Ok::<UserCtx, Rejection>(utx)
+				}
+				None => Err(Error::FailAuthMissingXAuth.into()),
+			}
+		})
 }
--- backend/src/web/mod.rs
+++ backend/src/web/mod.rs
@@ -32,6 +32,9 @@ pub async fn start_web(web_folder: &str, web_port: u16, db: Arc<Db>) -> Result<(
 pub enum Error {
 	#[error("Web server failed to start because web-folder '{0}' not found.")]
 	FailStartWebFolderNotFound(String),
+
+	#[error("Fail authentication missing X-Auth-Token header.")]
+	FailAuthMissingXAuth,
 }
 
 // region:    Warp Custom Error
